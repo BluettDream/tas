af722355a9b32953f92f2917ef0b2ac362ca1b3a
MengHao <907749695@qq.com>
2021-09-29 09:16:15 +0800

    feat:添加首页

---
+++ tas-web/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <h1>欢迎来到教学辅助系统</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Home'
+}
+</script>
+
+<style scoped>
+h1{
+    font-size: 50px;
+}
+</style>
